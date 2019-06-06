96d02a08176118946a2806f406aa5048339bbee1
马过 <mmmaaaggg@163.com>
2019-06-06 15:51:58 +0800

    use python -m

--- auto_record_account.bat
+++ auto_record_account.bat
@@ -1,1 +1,2 @@
-run_script period_task\auto_record_account_info.py
+::run_script period_task\auto_record_account_info.py
+C:\GitHub\QABAT\venv\Scripts\python.exe -m period_task.auto_record_account_info
